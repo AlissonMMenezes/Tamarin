dc2ad3b5e066b235cdf46348e35000eab4e131a3
Alisson Menezes <Alisson.itops@gmail.com>
2022-02-20 17:51:21 +0100

    refatoring to use OO

--- tamarin.py
+++ tamarin.py
@@ -9,142 +9,183 @@ from terminaltables import AsciiTable
 import os
 import shutil
 
-qemu_bin = shutil.which("qemu-system-x86_64")
-if not os.path.exists(".data"): os.mkdir(".data")
 
 
-class VirtualMachine:
-    def __init__():
-        pass
+
+class VirtualMachine(object):
+    def __init__(self,vm_config,ssh_user="",ssh_password=""):
+        self.qemu_bin = shutil.which("qemu-system-x86_64")
+        self.name = vm_config["name"]
+        self.memory = vm_config["memory"]
+        self.ssh_user = ssh_user
+        self.ssh_password = ssh_password
+        self.port_forward = vm_config["port-forward"]
+        if not os.path.exists(".data"): os.mkdir(".data")
+
+    def setup(self,name, ssh_port):
+        attempts = 0
+        print("[+] Setting up VM: {0} on port {1}".format(name,ssh_port))
+        while attempts < 3:
+            try: 
+                location = ("127.0.0.1", int(ssh_port))
+                a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                a_socket.connect(location)
+                banner = a_socket.recv(100)
+                if "SSH" in str(banner):
+                    print("[+] VM Ready!")
+                    break
+                else:
+                    print("[+] Waiting the VM to start")    
+                    time.sleep(10)                  
+            except Exception as e:
+                print("[!] Waiting port %s to be open"%ssh_port)
+                time.sleep(10)
+            attempts += 1
         
-class Tamarin:
-    def __init__():
-        pass
+        attempts = 0
+        while attempts < 3:
+            try:
+                ssh = paramiko.SSHClient()
+                ssh.load_system_host_keys()
+                ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                ssh.connect(hostname='127.0.0.1',port=ssh_port,username=self.ssh_user,password=self.ssh_password)
+                print("[+] Setting the hostname")
+                stdin,stdout,stderr = ssh.exec_command("hostnamectl set-hostname %s "%name)
+                if stderr.channel.recv_exit_status() != 0:
+                    stderr.read()
+                else:
+                    stdout.read()
+                break
+            except Exception as e:
+                print("[+] Waiting for an ssh connection") 
+                print(e)
+                time.sleep(10)
+            attempts += 1
 
-def help():
-    print("""
-    Usage:
-    tamarin.py [provision|destroy]
-    tamarin.py start [vm_name]
-    tamarin.py stop [vm_name]
-    tamarin.py destroy [vm_name]
-    tamarin.py list
-    tamarin.py help
-    """)
+    def create(self):
+        print("[+] Creating VM: {0}".format(self.name))
+        if not os.path.exists(".data/%s.qcow2"%self.name): shutil.copyfile("debian.qcow2", ".data/%s.qcow2"%self.name)
+        params = []
+        params.append("{0} --nographic --pidfile .data/{1}.pid".format(self.qemu_bin, self.name))
+        params.append("-m %s"%self.memory)
+        port_forward = ""
+        setup_port = 0
+        for p in self.port_forward:
+            host_port, guest_port = p.split(":")
+            if int(guest_port) == 22: setup_port = host_port
+            port_forward += ",hostfwd=tcp::{0}-:{1}".format(host_port,guest_port)
+        params.append("-nic user,id=net0"+port_forward)
+        params.append("-drive file=.data/%s.qcow2"%self.name)
+        qemu_cmd = " ".join(params)
+        #print(qemu_cmd)
+        output = subprocess.Popen([qemu_cmd],stdout=subprocess.PIPE,shell=True)       
+        self.setup(self.name, setup_port)
 
-def parse_yaml(file_name="Inventory.yml"):
-    with open(file_name, "r") as f:
-        inventory = yaml.load(f.read(),Loader=yaml.FullLoader)
-    return inventory
-
-def list_vms():
-    inventory = parse_yaml()
-    table_data = [
-            ['Name', 'status'],
-        ]
-    for vm in inventory["inventory"]["vms"]:
-        status = "Running" if os.path.exists(".data/{0}.pid".format(vm["name"])) else "Stopped"
-        table_data.append([vm["name"], status])
-    
-    table = AsciiTable(table_data)
-    print (table.table)
-
-def create_vm(vm):
-    print("[+] Creating VM: {0}".format(vm["name"]))
-    params = []
-    params.append("{0} --nographic --pidfile .data/{1}.pid".format(qemu_bin, vm["name"]))
-    params.append("-m %s"%vm["memory"])
-    port_forward = ""
-    for p in vm["port-forward"]:
-        host_port, guest_port = p.split(":")
-        port_forward += ",hostfwd=tcp::{0}-:{1}".format(host_port,guest_port)
-    params.append("-nic user,id=net0"+port_forward)
-    params.append("-drive file=/Users/alissonmachado/Documents/GitHub/Tamarin/disk2.qcow2")
-    output = subprocess.Popen([" ".join(params)],stdout=subprocess.PIPE,shell=True)
-    for p in vm["port-forward"]:
-        host_port, guest_port = p.split(":")
-        if int(guest_port) == 22:
-            provisioning(vm, host_port)
-            break
-
-def provisioning(vm,ssh_port):
-    while True:
-        a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        location = ("127.0.0.1", int(ssh_port))
-        result_of_check = a_socket.connect_ex(location)
-        if result_of_check == 0:
-            a_socket.close()
-            print("[+] VM Started!")
-            i = 0
-            while i < 3:
-                try: 
-                    a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    a_socket.connect(location)
-                    banner = a_socket.recv(100)
-                    if "SSH" in str(banner):
-                        print("[+] VM Ready!")
-                        break
-                    else:
-                        time.sleep(10)                  
-                except Exception as e:
-                    print(e)
-                    time.sleep(10)
-                i += 1
-            break
+    def destroy(self,vm):
+        if os.path.exists(".data/%s.qcow2"%vm["name"]): 
+            shutil.rmtree(".data/%s.qcow2"%vm["name"])
         else:
-            print("[+] Waiting the VM to start")    
-            time.sleep(35) 
+            print("[+] VM not provisioned or already destroyed")
 
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.load_system_host_keys()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(hostname='127.0.0.1',port=ssh_port,username='root',password='alisson')
-        stdin,stdout,stderr = ssh.exec_command("touch /file_provisioned.txt")
-        if stderr.channel.recv_exit_status() != 0:
-            stderr.read()
-        else:
-            stdout.read()
-    except Exception as e:
-        print("[+] Waiting for an ssh connection")            
+        
+class Tamarin(object):
+    def __init__(self,file_name="Inventory.yml"):
+        with open(file_name, "r") as f:
+            self.inventory = yaml.load(f.read(),Loader=yaml.FullLoader)
+            self.ssh_user = self.inventory["inventory"]["ssh-user"]
+            self.ssh_password = self.inventory["inventory"]["ssh-password"]
+
+    def usage(self):
+        print("""
+        Usage:
+        tamarin.py [provision|destroy]
+        tamarin.py start [vm_name]
+        tamarin.py stop [vm_name]
+        tamarin.py destroy [vm_name]
+        tamarin.py status
+        tamarin.py help
+        """)
+
+    def provision(self, vm, ssh_port):
+        attempts = 0
+        while attempts < 3:
+            try: 
+                location = ("127.0.0.1", ssh_port)
+                a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                a_socket.connect(location)
+                banner = a_socket.recv(100)
+                if "SSH" in str(banner):
+                    print("[+] VM Ready!")
+                    break
+                else:
+                    print("[+] Waiting the VM to start")    
+                    time.sleep(10)                  
+            except Exception as e:
+                print(e)
+                time.sleep(10)
+            attempts += 1
+
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.load_system_host_keys()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(hostname='127.0.0.1',port=ssh_port,username=self.ssh_user,password=self.ssh_password)
+            stdin,stdout,stderr = ssh.exec_command("echo %s > /etc/hostname"%vm["name"])
+            if stderr.channel.recv_exit_status() != 0:
+                stderr.read()
+            else:
+                stdout.read()
+            
+        except Exception as e:
+            print("[+] Waiting for an ssh connection")            
 
 
-def menu():
-    if len(sys.argv) < 2:
-        help()
-    elif sys.argv[1] == "start":
-        if len(sys.argv) > 2:
-            for vm in parse_yaml()["inventory"]["vms"]:
-                if vm["name"] == sys.argv[2]:
-                    t = threading.Thread(target=create_vm,args=(vm,))
+    def start(self,name=""):
+        for vm_config in self.inventory["inventory"]["vms"]:
+            if vm_config["name"] == sys.argv[2]:
+                    vm = VirtualMachine(vm_config, self.ssh_user, self.ssh_password)
+                    t = threading.Thread(target=vm.create,args=())
                     t.start()
                     while t.is_alive():
                         time.sleep(5)
                     t.join()
-                    break            
-            
-        else:
-            print("[+] Provisioning VMs")
-            for vm in parse_yaml()["inventory"]["vms"]: 
-                t = threading.Thread(target=create_vm,args=(vm,))
-                t.start()
-                while t.is_alive():
-                    time.sleep(5)
-                t.join()
-    elif sys.argv[1] == "stop":
-        with open(".data/{0}.pid".format(sys.argv[2])) as f:
+                    break  
+
+    def status(self):
+        table_data = [
+                ['Name', 'status'],
+            ]
+        for vm in self.inventory["inventory"]["vms"]:
+            status = "Running" if os.path.exists(".data/{0}.pid".format(vm["name"])) else "Stopped"
+            table_data.append([vm["name"], status])
+        
+        table = AsciiTable(table_data)
+        print (table.table)
+
+    def stop(self, vm):
+        with open(".data/{0}.pid".format(vm)) as f:
             pid = int(f.read())
             os.kill(pid, 1)
             print("[!] Shutting down VM: {0}".format(sys.argv[2]))
-    elif sys.argv[1] == "destroy":
-        with open(".data/{0}.pid".format(sys.argv[2])) as f:
-            pid = f.read()
-            print("[!] Shutting down VM: {0}".format(sys.argv[2]))
-    elif sys.argv[1] == "list":
-        list_vms()
-    else:
-        #os.mkdir(".data")
-        pass   
+
+    def destroy(self,vm):
+        if os.path.exists(".data/%s.qcow2"%vm): 
+            os.remove(".data/%s.qcow2"%vm)
+        else:
+            print("[+] VM not provisioned or already destroyed")
+
+    
 
 if __name__ == "__main__":
-    menu()    
+    tamarin = Tamarin()
+    if len(sys.argv) < 2:
+        tamarin.usage()
+    elif sys.argv[1] == "start":
+        tamarin.start(sys.argv[2])                     
+    elif sys.argv[1] == "stop":
+        tamarin.stop(sys.argv[2])
+    elif sys.argv[1] == "destroy":
+        tamarin.stop(sys.argv[2])
+        tamarin.destroy(sys.argv[2])
+    elif sys.argv[1] == "status":
+        tamarin.status()

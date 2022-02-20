da577984daad5cfed5ebc2ae66a8d96e54096670
Alisson Menezes <Alisson.itops@gmail.com>
2022-02-20 22:06:44 +0100

    build setup

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     name='alisson_cli',  
+     name='Tamarin',  
      version='0.1',
-     scripts=['tamarin.py'] ,
      author="Alisson Machado",
      author_email="alisson.itops@gmail.com",
      description="Infrastructure as code",
      long_description=long_description,
-   long_description_content_type="text/markdown",
+     long_description_content_type="text/markdown",
      url="https://github.com/AlissonMMenezes/Tamarin",
-     packages=setuptools.find_packages(),
      classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: Apache2 License",
-         "Operating System :: OS Independent",
-     ],
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
  )
